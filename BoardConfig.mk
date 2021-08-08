a3bc96f249dc0a66a187574111927c86244afaae
Alcatraz323 <alcatraz32323@gmail.com>
2021-08-08 21:48:18 +0800

    alphalm: use prebuilt images from stock and xda for test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,11 @@ DEVICE_PATH := device/lge/alphalm
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hardware=alphalm
 
+# Prebuilts
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/$(BOARD_KERNEL_IMAGE_NAME)
+
 # Recovery
 TARGET_RECOVERY_WIPE := $(DEVICE_PATH)/recovery/recovery.wipe
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.alphalm
