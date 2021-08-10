9dd52c01125dbc6e749e0fee72d955c075520d71
Alcatraz323 <alcatraz32323@gmail.com>
2021-08-10 09:31:05 +0800

    use prebuilts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,9 +23,10 @@ DEVICE_PATH := device/lge/alphalm
 BOARD_KERNEL_CMDLINE += androidboot.hardware=alphalm
 
 # Prebuilts
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+# BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/$(BOARD_KERNEL_IMAGE_NAME)
+# TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+# TARGET_FORCE_PREBUILT_KERNEL := true
 
 # Recovery
 TARGET_RECOVERY_WIPE := $(DEVICE_PATH)/recovery/recovery.wipe
